11498b773eb55c597acacf7f139b7e14f1ed19c7
Artur Cherniak <artur.cherniak@logicify.com>
2018-10-26 13:06:47 +0300

    fix

--- hackManager/test.py
+++ hackManager/test.py
@@ -4,6 +4,7 @@ import os
 
 class Test(object):
     """Base class utilized to test hacks on. Test(name, **kwargs)"""
+
     def __init__(self, name, **kwargs):
         self.run = True
         self.name = name
@@ -13,10 +14,11 @@ class Test(object):
                 exec("self.{0} = '{1}'".format(key, value))
             else:
                 exec("self.{0} = {1}".format(key, value))
+
     def mainloop_check(self, attr):
         while self.run:
-            print self.name, "->", eval("self.%s" %attr)
+            print(self.name, "->", eval("self.%s" % attr))
             time.sleep(1)
-    def __repr__(self):
-        return "<Test: %s>" %self.name
 
+    def __repr__(self):
+        return "<Test: %s>" % self.name
